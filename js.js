916065652b819a0bf45cdf57cce9210e49d0cef6
As-Ad Arik Azad <65552670+ARIK1910931@users.noreply.github.com>
2022-12-19 22:09:31 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,96 @@
+document.getElementById('btn-1').addEventListener('click', function () {
+
+
+
+     const playerName = document.getElementById('player-1');
+     const playerValue = playerName.innerText;
+
+
+
+
+
+     const Bestplayers = document.getElementById('Myplayer-1');
+     const p = document.createElement('p');
+     p.innerText = playerValue;
+     Bestplayers.appendChild(p);
+
+
+
+
+})
+
+document.getElementById('btn-2').addEventListener('click', function () {
+
+     const playerName = document.getElementById('player-2');
+     const playerValue = playerName.innerText;
+
+
+     const Bestplayers = document.getElementById('Myplayer-2');
+     const p = document.createElement('p');
+     p.innerText = playerValue;
+     Bestplayers.appendChild(p);
+
+
+})
+
+document.getElementById('btn-3').addEventListener('click', function () {
+
+     const playerName = document.getElementById('player-3');
+     const playerValue = playerName.innerText;
+
+
+     const Bestplayers = document.getElementById('Myplayer-3');
+     const p = document.createElement('p');
+     p.innerText = playerValue;
+     Bestplayers.appendChild(p);
+
+
+})
+
+document.getElementById('btn-4').addEventListener('click', function () {
+
+     const playerName = document.getElementById('player-4');
+     const playerValue = playerName.innerText;
+
+
+     const Bestplayers = document.getElementById('Myplayer-4');
+     const p = document.createElement('p');
+     p.innerText = playerValue;
+     Bestplayers.appendChild(p);
+
+
+
+})
+
+
+document.getElementById('btn-5').addEventListener('click', function () {
+
+     const playerName = document.getElementById('player-5');
+     const playerValue = playerName.innerText;
+
+
+     const Bestplayers = document.getElementById('Myplayer-5');
+     const p = document.createElement('p');
+     p.innerText = playerValue;
+     Bestplayers.appendChild(p);
+
+
+})
+
+
+document.getElementById('btn-6').addEventListener('click', function () {
+
+
+     const playerName = document.getElementById('player-6');
+     const playerValue = playerName.innerText;
+
+
+     const Bestplayers = document.getElementById('Myplayer-6');
+     const p = document.createElement('p');
+     p.innerText = playerValue;
+     Bestplayers.appendChild(p);
+
+
+})
+
+
